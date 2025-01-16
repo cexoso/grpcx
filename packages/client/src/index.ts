2849824189ee335314474350fe37c54cf5729bdb
xiongjie <jiexiong@futunn.com>
2025-01-16 20:48:14 +0800

    add dev for client

---
+++ packages/client/src/index.ts
@@ -0,0 +1,11 @@
+const headers = {
+  ":authority": "localhost:50051",
+  ":method": "POST",
+  ":path": "/helloworld.Greeter/SayHello",
+  ":scheme": "http",
+  "grpc-accept-encoding": "identity,deflate,gzip",
+  "accept-encoding": "identity",
+  "user-agent": "grpc-node-js/1.12.5",
+  "content-type": "application/grpc",
+  te: "trailers",
+};
