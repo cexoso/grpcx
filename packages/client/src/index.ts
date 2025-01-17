175c87d14d699bf2f0410fe9df38d70ec28ee1a0
xiongjie <jiexiong@futunn.com>
2025-01-17 20:26:19 +0800

    客户端和服务器端通信调通

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,4 +1,12 @@
-const headers = {
+import { connect } from "http2";
+import { encodeHelloRequest } from "./messages/helloworld";
+import { wrapEncode } from "@protobuf-es/core";
+import { Buffer } from "buffer";
+import { encode } from "@cexoso/grpc-utils";
+
+const client = connect("http://localhost:50051");
+
+const req = client.request({
   ":authority": "localhost:50051",
   ":method": "POST",
   ":path": "/helloworld.Greeter/SayHello",
@@ -7,5 +15,34 @@ const headers = {
   "accept-encoding": "identity",
   "user-agent": "grpc-node-js/1.12.5",
   "content-type": "application/grpc",
-  te: "trailers",
-};
+  // te: "trailers",
+});
+
+const encodeMessage = wrapEncode(encodeHelloRequest);
+const message = encodeMessage({
+  name: "world",
+});
+
+const x = encode(new Uint8Array(Buffer.from(message)));
+console.log(
+	'debugger 🐛',
+	x.buffer
+);
+
+req.write(x);
+req.end();
+
+let data = Buffer.from("");
+
+req.on("data", (chunk) => {
+  data = Buffer.concat([data, chunk]);
+});
+
+req.on("end", () => {
+  console.log(data.toString());
+  client.close();
+});
+
+req.on("trailers", (headers) => {
+  console.log("debugger 🐛 headers", headers);
+});
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,25 +1,62 @@
-import { createServer } from "http2";
-import { getBodyFromReq } from "./utils/get-body-from-req";
+import { createServer, constants } from "http2";
 import { wrapDecode, wrapEncode } from "@protobuf-es/core";
 import {
   decodeHelloRequest as _decodeHelloRequests,
   encodeHelloReply as _encodeHelloReply,
 } from "./messages/helloworld";
-import { decode, encode } from "./coding";
+import { encode, decode } from "@cexoso/grpc-utils";
 import { Buffer } from "buffer";
+constants.HTTP2_HEADER_STATUS;
 
 const decodeHelloRequest = wrapDecode(_decodeHelloRequests);
 const encodeHelloReply = wrapEncode(_encodeHelloReply);
 
-const server = createServer(async (req, res) => {
-  const body = await getBodyFromReq(req);
-  const message = decode(body);
-  const data = decodeHelloRequest(message);
-  const responsMessage = encodeHelloReply({
-    message: `hello ${data.name}`,
+const server = createServer();
+
+server.on("stream", (stream, headers) => {
+  console.log("debugger 🐛 headers", headers);
+  let data: Buffer[] = [];
+  stream.on("data", (buffer: Buffer) => {
+    data.push(buffer);
   });
 
-  res.end(encode(Buffer.from(responsMessage)));
+  stream.on("end", () => {
+    const req = Buffer.concat(data);
+    const message = decode(new Uint8Array(req));
+    const reqData = decodeHelloRequest(message);
+    const responsMessage = encodeHelloReply({
+      message: `hello ${reqData.name}`,
+    });
+
+    stream.respond(
+      {
+        ":status": 200,
+        "content-type": "application/grpc+proto",
+        "grpc-accept-encoding": "identity,deflate,gzip",
+        "grpc-encoding": "identity",
+      },
+
+      {
+        waitForTrailers: true,
+      },
+    );
+    stream.write(
+      encode(new Uint8Array(Buffer.from(responsMessage))),
+      (error) => {
+        console.log("res.stream.end();");
+        if (!error) {
+          stream.end();
+          stream.once("wantTrailers", () => {
+            console.log("debugger 🐛 sendTrailers");
+            stream.sendTrailers({
+              "grpc-message": "OK",
+              "grpc-status": 0,
+            });
+          });
+        }
+      },
+    );
+  });
 });
 const port = 50051;
 
