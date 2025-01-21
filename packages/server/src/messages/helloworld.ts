a4f23b6ea60c0aa087ff0ebdbc9f3bfe448f50bc
xiongjie <jiexiong@futunn.com>
2025-01-21 22:26:22 +0800

    update pb

--- packages/server/src/messages/helloworld.ts
+++ packages/server/src/messages/helloworld.ts
@@ -1,46 +1,72 @@
-import { readString, defineMessage, TagHandler, EncoderWithoutTag, encodeStringToBuffer } from '@protobuf-es/core'
+import { readString, defineMessage, TagHandler, EncoderWithoutTag, encodeStringToBuffer, readInt32, encodeInt32ToBuffer } from '@protobuf-es/core'
 export interface HelloRequest {
-  name?: string;
+  name?: string
 }
 
 export const decodeHelloRequest = defineMessage<HelloRequest>(
-  new Map<number, TagHandler>([
-    [1, { type: "scalar", decode: readString, name: "name" }],
-  ]),
-);
+  new Map<number, TagHandler>([[1, { type: 'scalar', decode: readString, name: 'name' }]])
+)
 
-export const encodeHelloRequest: EncoderWithoutTag<HelloRequest> = ({
-  value,
-  writer,
-}) => {
-  if (value["name"] !== undefined) {
+export const encodeHelloRequest: EncoderWithoutTag<HelloRequest> = ({ value, writer }) => {
+  if (value['name'] !== undefined) {
     encodeStringToBuffer({
-      value: value["name"],
+      value: value['name'],
       tag: 1,
       writer,
-    });
+    })
   }
-};
+}
+
+export interface GetCurrentUserReq {
+  uid?: number
+}
+
+export const decodeGetCurrentUserReq = defineMessage<GetCurrentUserReq>(
+  new Map<number, TagHandler>([[1, { type: 'scalar', decode: readInt32, name: 'uid' }]])
+)
+
+export const encodeGetCurrentUserReq: EncoderWithoutTag<GetCurrentUserReq> = ({ value, writer }) => {
+  if (value['uid'] !== undefined) {
+    encodeInt32ToBuffer({
+      value: value['uid'],
+      tag: 1,
+      writer,
+    })
+  }
+}
+
+export interface User {
+  name?: string
+}
+
+export const decodeUser = defineMessage<User>(
+  new Map<number, TagHandler>([[1, { type: 'scalar', decode: readString, name: 'name' }]])
+)
+
+export const encodeUser: EncoderWithoutTag<User> = ({ value, writer }) => {
+  if (value['name'] !== undefined) {
+    encodeStringToBuffer({
+      value: value['name'],
+      tag: 1,
+      writer,
+    })
+  }
+}
 
 export interface HelloReply {
-  message?: string;
+  message?: string
 }
 
 export const decodeHelloReply = defineMessage<HelloReply>(
-  new Map<number, TagHandler>([
-    [1, { type: "scalar", decode: readString, name: "message" }],
-  ]),
-);
+  new Map<number, TagHandler>([[1, { type: 'scalar', decode: readString, name: 'message' }]])
+)
 
-export const encodeHelloReply: EncoderWithoutTag<HelloReply> = ({
-  value,
-  writer,
-}) => {
-  if (value["message"] !== undefined) {
+export const encodeHelloReply: EncoderWithoutTag<HelloReply> = ({ value, writer }) => {
+  if (value['message'] !== undefined) {
     encodeStringToBuffer({
-      value: value["message"],
+      value: value['message'],
       tag: 1,
       writer,
-    });
+    })
   }
-};
+}
