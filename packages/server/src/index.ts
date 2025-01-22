99eb3de95a3b38cfc9ad69391bd11ba0599d2265
xiongjie <jiexiong@futunn.com>
2025-01-22 11:24:16 +0800

    使用 https 协议，否则浏览器无法建议连接

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,14 @@
-import { HelloApp } from "./app";
+import { HelloApp } from './app'
+import { createSecureServer } from 'http2'
+import { readFileSync } from 'fs'
 
-const app = HelloApp.createApp();
-const port = 50051;
-console.log(`listen on ${port}`);
-app.listen(port);
+const http2Server = createSecureServer({
+  key: readFileSync('cert/server.key'),
+  cert: readFileSync('cert/server.crt'),
+})
+const app = HelloApp.createApp({
+  http2Server,
+})
+const port = 50051
+console.log(`listen on ${port}`)
+app.listen(port)
